6e80c1af6f8a605f978c1fe7f757e46b49973e1e
kk-ruby <k.karasawa@kageyama-co.jp>
2022-02-07 08:02:50 +0900

    router_test

--- front/frontend/src/App.tsx
+++ front/frontend/src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// components
+import { Restaurants } from './containers/Restaurants';
+import { Foods } from './containers/Foods';
+import { Orders } from './containers/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ここを変えるよくあるやつ
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Switch>
+        // 店舗一覧ページ
+        <Route
+          exact
+          path="/restaurants">
+          <Restaurants />
+        </Route>
+        // フード一覧ページ
+        <Route
+          exact
+          path="/foods"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Foods />
+        </Route>
+        // 注文ページ
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
