dd9ccc013c05dbcbc08e6764d8e625ea8cf9fa29
kk-ruby <k.karasawa@kageyama-co.jp>
2022-02-20 14:14:32 +0900

    apiset

--- front/frontend/src/containers/Restaurants.jsx
+++ front/frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,14 @@
 import React, { Fragment } from 'react';
 
 export const Restaurants = () => {
+
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       レストラン一覧
